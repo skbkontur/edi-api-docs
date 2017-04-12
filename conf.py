5f7b98ec2e2e1c78a196ce45f26ea7fd04038825
Ivan <husfel@hotmail.com>
2017-04-12 16:02:50 +0500

    fix

--- conf.py
+++ conf.py
@@ -47,7 +47,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'API Контур.EDI'
+project = u'API Контур.EDI'
 author = 'Someone'
 
 # The version info for the project you're documenting, acts as replacement for
