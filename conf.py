47193be711969f4a5d261963513a9a238af07838
p.vostretsov <p.vostretsov@skbkontur.ru>
2018-06-28 12:21:29 +0500

    Try to remove local theme and add python package dependency

--- conf.py
+++ conf.py
@@ -23,6 +23,7 @@
 
 #
 #import sphinx_rtd_theme
+import sphinx_rtd_theme
 
 # -- General configuration ------------------------------------------------
 
@@ -96,7 +97,8 @@ html_favicon = '_static/favicon.ico'
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = ["_themes"]
+# html_theme_path = ["_themes"]
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
